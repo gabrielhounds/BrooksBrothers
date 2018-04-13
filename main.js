dd079c8abbf3c348910cef8cb34144500eccfdef
Patrick Batey <pbatey@patrickBatey-macBook.fios-router.home>
2018-04-13 13:34:56 -0400

    Intro updates

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ $(document).ready(function(){
 
 function init() {
 	console.log('init');
+
+	function random(min, max) {
+		if (max == null) { max = min; min = 0; }
+		return Math.round( Math.random() * (max - min) + min );
+	}
+
 	var t = TweenMax;
 
 	var frameNum = 1;
@@ -20,13 +26,64 @@ function init() {
 	var txt4 = $('.frame.frame4 .txt');
 	var txt5 = $('.frame.frame5 .txt');
 
+	var intro = $('#intro');
+
 	var arrowRight = $('#arrowRight');
 	var arrowLeft  = $('#arrowLeft');
 
 	$('.dot1').css({ backgroundImage : 'url(navDot_active.png)' });
-
 	t.set([frame2, frame2, frame3, frame4, frame5], { left : '-100%', opacity : 0 });
 	//t.set([txt1, txt2, txt3, txt4, txt5], { opacity : 0 });
+
+	var $width 			= window.innerWidth;
+	var $height 		= window.innerHeight;
+	var $area   		= $width * $height;
+
+	var $squareSize 	= $height / 7;
+
+	var $columnNum 		= Math.round($width  / $squareSize);
+	var $rowNum    		= Math.round($height / $squareSize);
+
+	var $squareWidth  	= $width / $columnNum;
+	var $squareHeight 	= $height / $rowNum;
+
+	var blueLogos  = ['bl_00.png', 'bl_01.png', 'bl_02.png', 'bl_03.png', 'bl_04.png', 'bl_05.png', 'bl_06.png', 'bl_07.png'];
+	var beigeLogos = ['bi_00.png', 'bi_01.png', 'bi_02.png', 'bi_03.png', 'bi_04.png', 'bi_05.png', 'bi_06.png', 'bi_07.png'];
+	var logos = [];
+
+	for (var i = 0; i < $columnNum; i++ ) {
+		for (var j = 0; j < $rowNum; j++ ) {
+
+			logos = $('<div>', { class : 'logos' }).appendTo('#intro');
+			$(logos).css( { width : $squareWidth, height : $squareHeight, left : $squareWidth * i + 'px' , top : $squareHeight * j + 'px' } );
+		}
+	}
+
+	var _logos = $('.logos').toArray();
+
+	$(_logos).each( function(i, el){
+		if (i % 2 === 0 ) {
+			//BEIGE BACKGROUND
+			$(el).css( { backgroundColor : '#b09870', backgroundImage:'url(' + blueLogos[random(0, blueLogos.length - 1)] + ')'} );
+			$(el).addClass('.move');
+			t.set(el, { left:'+=' + $squareWidth, opacity:0 });
+			t.to(el, 1.5, {left:'-=' + $squareWidth, opacity:1, ease:Expo.easeOut, delay:2});
+		} else {
+			//BLUE BACKGROUND
+			$(el).css( { backgroundColor : '#0b2036', backgroundImage:'url(' + beigeLogos[random(0, beigeLogos.length - 1)] + ')', opacity:0} );
+			t.to(el, 1.5, {opacity:1, ease:Expo.easeOut, delay:1.2});
+		}
+	});
+
+	var introBar = $('<div>', { id : 'introBar' }).appendTo('#intro');
+	$(introBar).css({ width : '100%' , height : $squareHeight * 3, top : $squareHeight * 2, backgroundColor : '#0b2036' , backgroundImage : 'url(introText.png)', backgroundSize : '80%', backgroundRepeat : 'no-repeat', backgroundPosition: 'center center '});
+
+	t.to('#introLogo', 0.6, {opacity : 0, ease:Expo.easeOut, delay:1.0});
+	t.from(introBar, 0.6, {opacity : 0, ease:Expo.easeOut, delay:4});
+	t.to(intro, 0.6, {autoAlpha : 0, ease:Expo.easeOut, delay:6});
+
+	//t.from('.move', 0.9, {left:'+=' + $squareWidth })
+	//t.set('.move', {opacity:0.5});
 
 	function handleRightClick(e) {
 		$('.dot').css({ backgroundImage : 'url(navDot.png)' });
@@ -143,46 +200,11 @@ function init() {
 		$('#main div').removeAttr('style');
 	});
 
-	var $width 			= window.innerWidth;
-	var $height 		= window.innerHeight;
-	var $area   		= $width * $height;
-	var $squareSize 	= 200;
-	var $columnNum 		= Math.ceil($width  / $squareSize);
-	var $rowNum    		= Math.ceil($height / $squareSize);
-	var $squareWidth  	= $width / $columnNum;
-	var $squareHeight 	= $height / $rowNum;
-	//var $wDif 		= $width /  arg.imageWidth;
-	//var $hDif 		= $height / arg.imageHeight;
-	//var $wOffset 		= (arg.imageWidth  - $width)  * $wDif;
-	//var $hOffset 		= (arg.imageHeight - $height) * $hDif;
-
-	var logos = [];
-
-	for (var i = 0; i < $columnNum; i++ ) {
-		for (var j = 0; j < $rowNum; j++ ) {
-
-			logos = $('<div>', { class : 'logos' }).appendTo('#intro');
-
-			$(logos).css( { width : $squareWidth, height : $squareHeight, left : $squareWidth * i + 'px' , top : $squareHeight * j + 'px' } );
 
 
-		}
-	}
 
-	//var _logos = $(logos).toArray();
 
-	//console.log(_logos[0]);
 
-	//$('.logos:odd').css( { backgroundColor : '#b09870'} );
-
-	$('.logos').each(function(i, el) {
-		if (i % 2 === 0) {
-			//$(el).html(i);
-			$(el).css( { backgroundColor : '#b09870'} );
-		} else {
-			$(el).css( { backgroundColor : '#0b2036'} );
-		}
-	});
 
 
 
